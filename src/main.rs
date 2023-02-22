7074386804ae751731fe648689246493182c0406
flooijdt <flooijdt@posteo.de>
2023-02-22 14:05:08 -0300

    default commit

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn order_weight(s: &str) -> String {
     // your code
     let mut vecko: Vec<Vec<char>> = Vec::new();
     let mut vecka: Vec<Vec<u32>> = Vec::new();
+    let mut veckb: Vec<u32> = Vec::new();
     let mut strr: Vec<&str> = s.split_whitespace().collect();
     for i in strr.iter_mut() {
         let mut vecky: Vec<char> = i.chars().collect();
@@ -13,11 +14,11 @@ fn order_weight(s: &str) -> String {
     }
     for v in vecko.iter_mut() {
         for c in v {
-            c.to_digit(10).unwrap();
+            veckb.push(c.to_digit(10).unwrap());
         }
-        //     vecka.push(v);
+        vecka.push(veckb.clone());
     }
     // vecka = vecko.into();
-    println!("{:?}", vecko);
+    println!("{:?}", vecka);
     "String".to_string()
 }
