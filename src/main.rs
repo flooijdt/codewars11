8c78d57a853d56147ab398a2dfb9bda893e10135
flooijdt <flooijdt@posteo.de>
2023-02-22 12:17:38 -0300

    default commit

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,17 @@ fn main() {
 fn order_weight(s: &str) -> String {
     // your code
     let mut vecko: Vec<Vec<char>> = Vec::new();
+    let mut vecka: Vec<u32> = Vec::new();
     let mut strr: Vec<&str> = s.split_whitespace().collect();
     for i in strr.iter_mut() {
         let mut vecky: Vec<char> = i.chars().collect();
         vecko.push(vecky);
     }
-    println!("{:?}", vecko);
+    for v in vecko.iter_mut() {
+        for c in v {
+            vecka.push(c.to_digit(10).unwrap());
+        }
+    }
+    println!("{:?}", vecka);
     "String".to_string()
 }
